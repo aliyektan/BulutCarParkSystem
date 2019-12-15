0262134aeb97fdd73c74b95660470f6cd1c57d11
yektan <yektan@outlook.com>
2019-12-15 22:05:20 +0300

    Pricing period endpoint path changed.

--- src/main/java/com/aliyektan/bulut/controller/PricingPeriodController.java
+++ src/main/java/com/aliyektan/bulut/controller/PricingPeriodController.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path = "/pricing-types")
+@RequestMapping(path = "/pricing-periods")
 public class PricingPeriodController {
 
     @Autowired
